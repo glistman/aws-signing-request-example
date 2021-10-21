5155ae0f4fb6215a3751744473789ab8eee5af5a
listman <listman2390@gmail.com>
2021-10-20 22:09:52 -0500

    Initial Version

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use aws_signing_request::request::{
+    CanonicalRequestBuilder, AUTHORIZATION, X_AMZ_CONTENT_SHA256, X_AMZ_DATE,
+};
+use aws_signing_request::request::{AWS_JSON_CONTENT_TYPE, X_AWZ_TARGET};
+use chrono::Utc;
+
+#[tokio::main]
+async fn main() {
+    let host = "ingest.timestream.us-east-1.amazonaws.com";
+    let body = "{}";
+    let aws_access_key_id = env!("AWS_ACCESS_KEY_ID");
+    let aws_secret_access_key = env!("AWS_SECRET_ACCESS_KEY");
+
+    let mut canonical_request_builder = CanonicalRequestBuilder::new(
+        host,
+        "POST",
+        "/",
+        aws_access_key_id,
+        aws_secret_access_key,
+        "us-east-1",
+        "timestream",
+    );
+
+    let canonical_request = canonical_request_builder
+        .header("Content-Type", AWS_JSON_CONTENT_TYPE)
+        .header(X_AWZ_TARGET, "Timestream_20181101.DescribeEndpoints")
+        .body(body)
+        .build(Utc::now());
+
+    let client = reqwest::Client::new();
+
+    let enpoint = client
+        .post(format!("https://{}", host))
+        .header(X_AMZ_DATE, &canonical_request.date.iso_8601)
+        .header("Content-Type", AWS_JSON_CONTENT_TYPE)
+        .header(X_AWZ_TARGET, "Timestream_20181101.DescribeEndpoints")
+        .header(X_AMZ_CONTENT_SHA256, &canonical_request.content_sha_256)
+        .header(
+            AUTHORIZATION,
+            &canonical_request
+                .calculate_authorization()
+                .expect("Authorization creation failed"),
+        )
+        .body(body)
+        .send()
+        .await
+        .expect("Service error")
+        .text()
+        .await
+        .unwrap();
+
+    println!("{:?}", enpoint);
+}
